9d80f4c932f2516b6e902767b7425f157c486e6c
Björn Meyer <bjoern.meyer@flagbit.de>
2021-01-04 10:11:46 +0100

    category filter, templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
     darkMode: 'media',
@@ -6,29 +7,57 @@ module.exports = {
         colors: {
             gray: colors.gray,
             white: colors.white,
-            "sw-background": 'var(--background-color)',
-            "sw-border": 'var(--border-color)',
-            "sw-buy-button": 'var(--color-buy-button)',
-            "sw-buy-button-text": 'var(--color-buy-button-text)',
-            "sw-danger": 'var(--color-danger)',
-            "sw-info": 'var(--color-info)',
-            "sw-price": 'var(--color-price)',
-            "sw-primary": 'var(--color-primary)',
-            "sw-secondary": 'var(--color-secondary)',
-            "sw-success": 'var(--color-success)',
-            "sw-warning": 'var(--color-warning)',
-            "sw-headline": 'var(--headline-color)',
-            "sw-navigation-active": 'var(--navigation-active-color)',
-            "sw-navigation-hover": 'var(--navigation-hover-color)',
-            "sw-text": 'var(--text-color)',
+            'sw-background': 'var(--background-color)',
+            'sw-border': 'var(--border-color)',
+            'sw-buy-button': 'var(--color-buy-button)',
+            'sw-buy-button-text': 'var(--color-buy-button-text)',
+            'sw-danger': 'var(--color-danger)',
+            'sw-info': 'var(--color-info)',
+            'sw-price': 'var(--color-price)',
+            'sw-primary': 'var(--color-primary)',
+            'sw-secondary': 'var(--color-secondary)',
+            'sw-success': 'var(--color-success)',
+            'sw-warning': 'var(--color-warning)',
+            'sw-headline': 'var(--headline-color)',
+            'sw-navigation-active': 'var(--navigation-active-color)',
+            'sw-navigation-hover': 'var(--navigation-hover-color)',
+            'sw-text': 'var(--text-color)',
         },
         extend: {
             maxWidth: {
+                '3xs': '6rem',
                 'xxs': '12rem'
+            },
+            minWidth: {
+                '0': '0',
+                '25': '25%',
+                '50': '50%',
+                '75': '75%',
+                'full': '100%',
+                '150': '10rem',
+                '300': '20rem'
             }
         },
     },
     variants: {},
-    plugins: []
+    plugins: [
+        plugin(function ({ addComponents, theme }) {
+            const filter = {
+                '.filter-active': {
+                    padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+                    marginRight: `${theme('spacing.2')}`,
+                    borderRadius: theme('borderRadius.md'),
+                    fontWeight: theme('fontWeight.600'),
+                    backgroundColor: theme('colors.sw-border'),
+                    color: theme('colors.white'),
+                    '&:hover': {
+                        backgroundColor: theme('colors.sw-border')
+                    }
+                }
+            }
+
+            addComponents(filter)
+        })
+    ]
 }
 
